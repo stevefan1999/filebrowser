a06e3733f9e6db3af8835f8aeb4297ce7230d7f1
Fan Chun Yin <19037626d@connect.polyu.hk>
2021-07-29 12:40:55 +0800

    add brotli for better compression

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,36 @@
+const zlib = require("zlib");
 const CompressionPlugin = require("compression-webpack-plugin");
+
+const algorithms = {
+  brotli: {
+    filename: "[file].br[query]",
+    algorithm: "brotliCompress",
+    include: /\.(js|css|html|svg|json|ttf|eot|woff2?)(\?.*)?$/i,
+    compressionOptions: {
+      params: {
+        [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+      },
+    },
+    minRatio: 0.8,
+  },
+  gzip: {
+    filename: "[file].gz[query]",
+    algorithm: "gzip",
+    include: /\.(js|css|html|svg|json|ttf|eot|woff2?)(\?.*)?$/i,
+    minRatio: 0.8,
+  },
+};
 
 module.exports = {
   runtimeCompiler: true,
   publicPath: "[{[ .StaticURL ]}]",
-  parallel: 2,
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        include: /\.js$/,
-        deleteOriginalAssets: true,
-      }),
-    ],
+
+  chainWebpack(config) {
+    for (const [name, algorithm] of Object.entries(algorithms)) {
+      config
+        .plugin(`${name}-compression`)
+        .use(CompressionPlugin)
+        .init((Plugin) => new Plugin(algorithm));
+    }
   },
 };
